6e44a59e14d20b63dfaab3cbdc8f679a3aafafa9
Ha <muninnhugin@gmail.com>
2023-11-24 20:33:59 -0800

    add toasts after buttons pressed

--- sleeptracker/src/app/home/home.page.ts
+++ sleeptracker/src/app/home/home.page.ts
@@ -25,7 +25,6 @@ export class HomePage implements OnInit{
 	}
 
 	ngOnInit() {
-		console.log(this.allSleepData);
     this.sleepBtn = document.getElementById('sleep_btn');
     this.wakeBtn = document.getElementById('wake_btn');
     if(this.wakeBtn != null) this.wakeBtn.style.display = 'none';
