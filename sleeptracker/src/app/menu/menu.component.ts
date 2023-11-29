e5d5227107205b10a23b088b1769c6304337cd1f
Rongbing Lai <rongbil1@uci.edu>
2023-11-28 22:55:43 -0800

    tab bar navigate fix

--- sleeptracker/src/app/menu/menu.component.ts
+++ sleeptracker/src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -7,8 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {}
+
+  sleepiness():void {
+    this.router.navigate(['/sleepiness']);
+  }
+
+  home():void {
+    this.router.navigate(['/home']);
+  }
+
+  log():void {
+    this.router.navigate(['/log']);
+  }
 
 }
